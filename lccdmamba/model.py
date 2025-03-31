a17295528acaf087194cff1cdbe5d8cb4ffd0536
juncyan <125954099+juncyan@users.noreply.github.com>
2025-03-31 16:34:19 +0800

    Update model.py

--- lccdmamba/model.py
+++ lccdmamba/model.py
@@ -1,46 +1,48 @@
-import os
-import time
-import math
-import copy
-from functools import partial
-from typing import Optional, Callable, Any
-from collections import OrderedDict
-
-
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from .unireplknet import  unireplknet_s
+from .modules import LKFA, BCIF
 
-from .decoder import Decoder
-from .backbone import Backbone_VSSM #, Backbone_VSSM_samll
-
-
-class LCCDMamba(nn.Module):
-    def __init__(self):
+# RepLK Convolutional Additive Mamba for Land Cover Fain-graind Understanding
+class LKMamba_CD(nn.Module):
+    def __init__(self, num_cls=2) -> None:
         super().__init__()
-        self.encoder = Backbone_VSSM(out_indices=(0, 1, 2, 3))
-        self.decoder = Decoder(
-            dims=self.encoder.dims,
-            out_channels=64,
-            channel_first=self.encoder.channel_first)
+        self.encoder = unireplknet_s()
+        self.encoder.eval()
+        self.encoder.reparameterize_unireplknet()
 
-        self.main_clf = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
+        self.bf4 = BCIF(768, 64)
+        self.bf2 = BCIF(192, 64)
 
+        self.up1 = LKFA(64)
+        # self.conv1 = nn.Sequential(nn.Conv2d(64,64,1), nn.BatchNorm2d(64), nn.ReLU(), nn.Conv2d(64,64,3,1,1), nn.BatchNorm2d(64), nn.ReLU())
+        self.up2 = LKFA(64)
 
-    def forward(self, pre_data, post_data=None):
-        if post_data == None:
-            post_data = pre_data[:,3:,:,:]
-            pre_data = pre_data[:,:3,:,:]
+        self.conv2 = nn.Sequential(nn.Conv2d(64,64,3,1,1), nn.BatchNorm2d(64), nn.ReLU())
+
+        self.cls = nn.Conv2d(64, 2, 7, padding=3)
+
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+
+    def forward(self, x1, x2):
+        f1_list = self.encoder(x1)
+        f2_list = self.encoder(x2)
         
-        pre_features = self.encoder(pre_data)
-        post_features = self.encoder(post_data)
+        p4, p2 = f1_list[3], f1_list[1]
+        b4, b2 = f2_list[3], f2_list[1]
         
-        output = self.decoder(pre_features, post_features)
-
-        output = F.interpolate(output, size=pre_data.size()[-2:], mode='bilinear')
-        output = self.main_clf(output)
-
-        return output
-    
+        f4 = self.bf4(p4, b4).contiguous()
+        f2 = self.bf2(p2, b2).contiguous()
+        
+        f4 = self.up1(f4)
+        # f4 = self.conv1(f4)
+        f4 = F.interpolate(f4, f2.shape[-2:], mode='bilinear')
+        f = self.conv2(f4+f2)
+        f = self.up2(f)
+        f = F.interpolate(f, scale_factor=8, mode='bilinear')
+        # f = self.conv2(f)
+        f = F.sigmoid(self.cls(f))
+        return f
